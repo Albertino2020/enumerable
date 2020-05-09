bab0481bec0f87ab5baed03757c218400110679f
Albertino Francisco <albertino@ua.pt>
2020-05-09 16:35:57 +0100

    Added comments and description.

--- enum.rb
+++ enum.rb
@@ -2,6 +2,9 @@
 
 #:nodoc:
 module Enumerable
+  # *********************************
+  # Beginning of my_each method.
+  # *********************************
   def my_each
     to_enum :my_each unless block_given?
     0.upto(length - 1) do |index|
@@ -9,15 +12,36 @@ module Enumerable
     end
   end
 
+  # *********************************
+  # End of my_each method.
+  # *********************************
+
+  # *********************************
+  # Beginning of my_each_with_index method.
+  # *********************************
   def my_each_with_index
     to_enum :my_each_with_index unless block_given?
     0.upto(length - 1) do |index|
       yield self[index], index
     end
   end
+
+  # *********************************
+  # End of my_each_with_index method.
+  # *********************************
 end
 
+# *********************************
+# RUNNING TESTS BELOW
+# *********************************
+# *********************************
+# Running my_each method test.
+# *********************************
 print([1, 2, 3, 4, 5].my_each { |num| puts num * 2 })
+
+# ***************************************
+# Running my_each_with_index method test.
+# ***************************************
 hash = {}
 %w[cat dog wombat].my_each_with_index do |item, index|
   hash[item] = index
