89fae22fedbcbc6ea271b493402714fea38d41b9
Albertino Francisco <albertino@ua.pt>
2020-05-09 16:23:31 +0100

    Updated my_each and my_each_with_index methods.

--- enum.rb
+++ enum.rb
@@ -1,13 +1,16 @@
 # frozen_string_literal: true
 
+#:nodoc:
 module Enumerable
   def my_each
+    to_enum :my_each unless block_given?
     0.upto(length - 1) do |index|
       yield self[index]
     end
   end
 
   def my_each_with_index
+    to_enum :my_each_with_index unless block_given?
     0.upto(length - 1) do |index|
       yield self[index], index
     end
