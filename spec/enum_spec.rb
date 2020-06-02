1fd58ad082a5d582a42911562242c2a2f2c7416f
Mupa1 <mupasmail@gmail.com>
2020-06-02 11:18:45 +0200

    add tests for my_each_with_index method

--- spec/enum_spec.rb
+++ spec/enum_spec.rb
@@ -1,18 +1,32 @@
-require "./enum.rb"
+# frozen_string_literal: true
 
-describe Enumerable do 
-    let(:arr) {[1, 2, 3, 4, 5, 6]}
-  describe "#my_each" do
-   
-    context "If a block is not given" do
-      it "returns enumerable" do
+require './enum.rb'
+
+describe Enumerable do
+  let(:arr) { [1, 2, 3, 4, 5, 6] }
+  describe '#my_each' do
+    context 'If a block is not given' do
+      it 'returns enumerable' do
         expect(arr.my_each).to be_kind_of(Enumerator)
       end
     end
-    context "if a block is given" do
-        it "returns self" do
-            expect(arr.my_each {|x| x ** 2}).to eql(arr)
-        end
+    context 'if a block is given' do
+      it 'returns self' do
+        expect(arr.my_each { |x| x**2 }).to eql(arr)
+      end
+    end
+  end
+
+  describe '#my_each_with_index' do
+    context 'If a block is not given' do
+      it 'returns enumerable' do
+        expect(arr.my_each_with_index).to be_kind_of(Enumerator)
+      end
+    end
+    context 'if a block is given' do
+      it 'returns self' do
+        expect(arr.my_each_with_index { |x, index| [x**2, index] }).to eql(arr)
+      end
     end
   end
 end
