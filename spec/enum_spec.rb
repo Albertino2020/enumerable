d69f8ccd24ab8c4845d21729dd48ef226e07a340
Mupa1 <mupasmail@gmail.com>
2020-06-02 12:16:18 +0200

    add tests for my_all methods

--- spec/enum_spec.rb
+++ spec/enum_spec.rb
@@ -4,6 +4,9 @@ require './enum.rb'
 
 describe Enumerable do
   let(:arr) { [1, 2, 3, 4, 5, 6] }
+  let(:arr2) { [1, nil, 3, 4, 5, 6] }
+  let(:arr3) { [1, 2, 3, 4, false, 6] }
+
   describe '#my_each' do
     context 'If a block is not given' do
       it 'returns enumerable' do
@@ -38,13 +41,45 @@ describe Enumerable do
     end
     context 'if a block is given' do
       it 'returns even elements' do
-        expect(arr.my_select { |x| x % 2 == 0 }).to eql([2, 4, 6])
+        expect(arr.my_select(&:even?)).to eql([2, 4, 6])
       end
       it 'returns odd elements' do
-        expect(arr.my_select { |x| x % 2 != 0 }).to eql([1, 3, 5])
+        expect(arr.my_select(&:odd?)).to eql([1, 3, 5])
       end
       it 'returns the array of elements whose square are less than 10' do
-        expect(arr.my_select { |x| x ** 2 < 10 }).to eql([1, 2, 3])
+        expect(arr.my_select { |x| x**2 < 10 }).to eql([1, 2, 3])
+      end
+    end
+  end
+
+  describe '#my_all?' do
+    context 'if a block is given' do
+      it 'returns false if all elements are divisible by 2' do
+        expect(arr.my_all?(&:even?)).to eql(false)
+      end
+      it 'returns false if all elements are not divisible by 2' do
+        expect(arr.my_all?(&:odd?)).to eql(false)
+      end
+      it 'returns true if all elements squared are less or equal to 36' do
+        expect(arr.my_all? { |x| x**2 <= 36 }).to eql(true)
+      end
+      it 'returns false if the array is empty' do
+        expect([].my_all?(&:odd?)).to eql(true)
+      end
+    end
+
+    context 'if a block is not given' do
+      it 'returns true if no element is nil or false' do
+        expect(arr.my_all?).to eql(true)
+      end
+      it 'returns false if an element is nil or false' do
+        expect(arr2.my_all?).to eql(false)
+      end
+      it 'returns false if an element is nil or false' do
+        expect(arr3.my_all?).to eql(false)
+      end
+      it 'returns true' do
+        expect([].my_all?).to eql(true)
       end
     end
   end
